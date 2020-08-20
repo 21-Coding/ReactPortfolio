e526cbf076f1585ece2d85202b722cd780dcf93f
Dom <dom@Doms-iMac.local>
2020-08-20 12:40:05 -0700

    add name to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,11 @@ import React from "react";
 function Header() {
 
     return (
+        <div className="header">
+            <h1>Dominique Youmans</h1>
+        </div>
 
-    )
+    );
 }
 
 export default Header;
