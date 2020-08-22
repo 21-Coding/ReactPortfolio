62f92a52911ab4cd3c2e3c99c3fffd24b0b6b565
Dom <dom@Doms-iMac.local>
2020-08-21 20:57:25 -0700

    add links to projects.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,11 +6,19 @@ function Projects() {
     <div>
       <h1>Current Work</h1>
       <li>
-        <a>DevShortcuts</a>
-        <a>MoneyHeist</a>
-        <a>PinballLocator</a>
-        <a>StarvingMarvins</a>
+        <a href="https://github.com/21-Coding/team-week-1">DevShortcuts</a>
       </li>
+
+      <li>
+        <a href="https://github.com/21-Coding/effective-spork">MoneyHeist</a>
+      </li>
+      <li>
+        <a href="https://github.com/drakewilcox/PinTrackerMVC.Solution">PinballLocator</a>
+      </li>
+      <li>
+        <a href="https://github.com/21-Coding/StarvingMarvins">StarvingMarvins</a>
+      </li>
+
 
 
     </div>
