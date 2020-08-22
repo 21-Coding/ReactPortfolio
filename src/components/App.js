fc20da635fbdbb94a8f64f89af6c93cf413d76e6
Dom <dom@Doms-iMac.local>
2020-08-21 23:44:15 -0700

    add best practice.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './Header.js'
-import Projects from './Projects.js'
+import Header from './Header'
+import Projects from './Projects'
 import './App.css';
 
 
