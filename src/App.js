1223867c7d944f6222a9305f7031ab7fa646968f
Dom Youmans <ddyoumans@icloud.com>
2020-08-22 18:39:47 -0700

    add react MDL package.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import Header from './Header'
-import Projects from './Projects'
+
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Projects />
+<h1>Hello World</h1>
     </div>
   );
 }
