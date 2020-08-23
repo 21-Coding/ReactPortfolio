1d5d631c960d3043b4fc369d264619601d419974
Dom Youmans <ddyoumans@icloud.com>
2020-08-22 18:59:51 -0700

    add navigation layout to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl'
 
 
-function App() {
+
+class App extends Component {
+  render(){
   return (
     <div className="App">
-<h1>Hello World</h1>
+{/* Uses a header that scrolls with the text, rather than staying locked at the top */}
+<div className="demo-big-content">
+    <Layout>
+        <Header title="Title" scroll>
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+        </Content>
+    </Layout>
+</div>
     </div>
   );
+}
 }
 
 export default App;
