9b776f699013dc682476936ae55c4dc2beca2190
Dom Youmans <ddyoumans@icloud.com>
2020-08-24 08:56:17 -0700

    add routing and import statements.

--- src/components/main.js
+++ src/components/main.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import LandingPage from './landingpage';
+import AboutMe from './aboutme';
+import Contacts from './contact';
+import Education from './education';
+import Experience from './experience';
+import Projects from './projects';
+import Resume from './resume';
+import Skills from './skills';
+
 import { Switch, Route } from 'react-router-dom';
 const Main = () => (
     <Switch>
         <Route exact path="/" component={LandingPage} />
+        <Route path="/aboutme" component={AboutMe} />
+        <Route path="/contacts" component={Contacts} />
+        <Route path="/education" component={Education} />
+        <Route path="/experience" component={Experience} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/skills" component={Skills} />
     </Switch>
 )
 
