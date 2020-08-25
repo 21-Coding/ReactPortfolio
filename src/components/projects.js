805d25cd06e503f3f518ce409da16e82429e6eae
Dom Youmans <ddyoumans@icloud.com>
2020-08-24 18:40:37 -0700

    add non-compiling code, error in projects.js.

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0};
+    } 
+
     render() {
         return(
-            <div><h1>Projects</h1></div>
-        )
+          <div>
+            <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+              <Tab>React</Tab>
+              <Tab>Angular</Tab>
+              <Tab>VueJS</Tab>
+              <Tab>MongoDB</Tab>
+            </Tabs>
+            )
     }
 }
 
