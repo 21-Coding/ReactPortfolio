5675164db3051922504d9bc9a07a6504b11c3a62
Dom Youmans <ddyoumans@icloud.com>
2020-08-25 06:06:58 -0700

    add cards.

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
     constructor(props) {
@@ -10,7 +10,17 @@ class Projects extends Component {
     toggleCategories() {
         if(this.state.activeTab === 0){
             return(
-                <div><h1>This is react</h1></div>
+                <Card shadow={5} stlye={{minWidth: '450', margin: 'auto'}}>
+                 <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Ffree-png-cgbgg&psig=AOvVaw1hVRedMwXsLtMGuU-nYpM8&ust=1598446660193000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJDt6IW0tusCFQAAAAAdAAAAABAE)'}}></CardTitle>
+                     <CardText>
+                         Lorem ipsum
+                     </CardText>
+                     <CardActions border>
+                         <Button colored>GitHub</Button>
+                         <Button colored>LinkedIn</Button> 
+                     </CardActions>
+
+                </Card>
             )
         } else if(this.state.activeTab === 1) {
             return (
@@ -37,6 +47,11 @@ class Projects extends Component {
             </Tabs>
 
             <section className="projects-grid">
+                <Grid className="projects-grid">
+                    <Cell col={12}>
+        <div className="content">{this.toggleCategories()}</div>
+                    </Cell>
+                </Grid>
                 {this.toggleCategories()}
             </section>
           </div>  
