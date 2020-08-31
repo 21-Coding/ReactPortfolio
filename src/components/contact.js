dbaf509c87c69b5f5d2b247c1abfbb6717a0c0ea
Dom Youmans <ddyoumans@icloud.com>
2020-08-30 22:46:33 -0700

    add avatar, name, and new import statements.

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Contact extends Component {
     render() {
         return(
-            <div><h1>Contact</h1></div>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Dom Youmans</h2>
+                        <img
+                            src=""
+                            alt="avatar"
+                            style={{height: '250px'}}
+                            />
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}></p>
+
+                    </Cell>
+                </Grid>
+            </div>
+         
         )
     }
 }
