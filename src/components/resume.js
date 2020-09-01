8aa977644a3794ce21e6b5d86290934ad01cd126
Dom Youmans <ddyoumans@icloud.com>
2020-08-31 22:12:01 -0700

    add new import statement for Education component.

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
+import Education from './education';
+
 class Resume extends Component {
     render() {
         return(
