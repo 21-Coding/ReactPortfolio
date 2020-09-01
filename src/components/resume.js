9fc5568d8ed0eaa1647a54459067577547ef6ce0
Dom Youmans <ddyoumans@icloud.com>
2020-08-31 22:13:45 -0700

    add mew import statement for Skills component.

--- src/components/resume.js
+++ src/components/resume.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 import Education from './education';
 import Experience from './experience';
+import Skills from './skills';
+
+
 class Resume extends Component {
     render() {
         return(
